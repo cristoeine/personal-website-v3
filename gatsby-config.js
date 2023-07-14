cdc015039be98529ce9201a6a1d14c9d579e87d0
Ferlly Wiyanto <ferlly.wiyanto@gmail.com>
2023-07-14 23:30:15 +0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `website_v4`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
